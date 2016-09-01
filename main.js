dc4e7ef224fed86b3f2e5ef998b9a5dfc828f79e
Vasylyk Bogdan <Vasylyk.bogdan@gmail.com>
2016-09-01 22:29:33 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,72 +6,50 @@
 var personalData = {};
 
 function personalDataHandler () {
-	personalData.height = +document.forms[0].elements.height.value;
-	personalData.weight = +document.forms[0].elements.weight.value;
-	personalData.age = +document.forms[0].elements.age.value;
-	personalData.sexWoman = document.getElementById('sexWoman').checked;
-	personalData.sexMan = document.getElementById('sexMan').checked;	
-	personalData.formulaHB = document.getElementById('Harris-ben').checked;
-	personalData.formulaMJ = document.getElementById('Mifflin-jeor').checked;
+	var formList0 = document.forms[0];
+	personalData.height = +formList0.height.value;
+	personalData.weight = +formList0.weight.value;
+	personalData.age = +formList0.age.value;
+	personalData.sex = formList0.sex.value;
+	personalData.formula = formList0.formula.value;
+	personalData.target = formList0.target.value;
+	personalData.activity_level = +formList0.activity_level.value;
 
-	function takeTarget () {
-		var target = document.getElementsByName('target')
-		if(target[0].checked){
-			return 0.85;		
-		}
-		if(target[1].checked){
-			return 1;
-		}
-		if(target[2].checked){
-			return 1.15;
-		};
-	};
-	personalData.target = takeTarget ();
-
-	function takeActivityLevel () {
-		var select = document.forms[0].elements.activity_level;
-		for (var i = 0; i < select.options.length; i++) {
-			var option = select.options[i];
-			if(option.selected) {
-				personalData.activity_level = option.value;
-			}
-		}
-		return personalData.activity_level;
-	};
-	takeActivityLevel();
-
+	
 	for (var key in personalData) {
-
 		console.log(key + " : " + personalData[key])
-
 	}
-
-	var voo = 0
+	var activ_target = personalData.target*personalData.activity_level;
+	var voo = 0;
 	function calculateCalories () {
-		if (personalData.sexWoman == true && personalData.formulaHB == true) {
-			var voo = (655.1 + 9.6 * personalData.weight + 1.85 * personalData.height - 4.68 * personalData.age) * personalData.activity_level * takeTarget();
+		if (personalData.sex == 'woman' && personalData.formula == 'HB') {
+			var voo = (655.1 + 9.6 * personalData.weight + 1.85 * personalData.height - 4.68 * personalData.age) 
+			* activ_target;
 
-		} else if (personalData.sexMan == true && personalData.formulaHB == true) {
-			voo = (66.47 + 13.75 * personalData.weight + 5.00 * personalData.height - 6.74 * personalData.age) * personalData.activity_level * takeTarget();
+		} else if (personalData.sex == 'man' && personalData.formula == 'HB') {
+			voo = (66.47 + 13.75 * personalData.weight + 5.00 * personalData.height - 6.74 * personalData.age) 
+			* activ_target;
 
-		} else if (personalData.sexWoman == true && personalData.formulaMJ == true) {
-			voo = (9.99 * personalData.weight + 6.25 * personalData.height - 4.92 * personalData.age - 161) * personalData.activity_level * takeTarget();
+		} else if (personalData.sex == 'woman' && personalData.formula == 'MJ') {
+			voo = (9.99 * personalData.weight + 6.25 * personalData.height - 4.92 * personalData.age - 161) 
+			* activ_target;
 
-		} else if (personalData.sexMan == true && personalData.formulaMJ == true) {
-			voo = (9.99 * personalData.weight + 6.25 * personalData.height - 4.92 * personalData.age + 5) * personalData.activity_level * takeTarget();
+		} else if (personalData.sex == 'man' && personalData.formula == 'MJ') {
+			voo = (9.99 * personalData.weight + 6.25 * personalData.height - 4.92 * personalData.age + 5) 
+			* activ_target;
 
 		}
 		var calories = document.getElementById('result');
 		var amount_cal = document.getElementById('amount_cal');
-		calories.innerHTML = Math.round(voo);
-		amount_cal.innerHTML = Math.round(voo);
-
 		var amount_prot = document.getElementById('amount_prot'); 
 		var amount_fat = document.getElementById('amount_fat');
 		var amount_carb = document.getElementById('amount_carb');
+
+		calories.innerHTML = Math.round(voo) + ' ккал';
+		amount_cal.innerHTML = Math.round(voo);
 		amount_prot.innerHTML = Math.round(voo*0.4/4.1);
 		amount_fat.innerHTML = Math.round(voo*0.15/9.3);
-		amount_carb.innerHTML = Math.round(voo*0.45/4.1 );
+		amount_carb.innerHTML = Math.round(voo*0.45/4.1);
 
 
 	};
@@ -134,7 +112,7 @@ function changePage (event) {
 
 };	
 
-var navigation = document.getElementById("navbar");
+var navigation = document.getElementById("menu");
 navigation.addEventListener("click", changePage);
 
 // відображення таблиці
@@ -203,7 +181,7 @@ tableCreator.addEventListener("click", loadProducts);
 
 function addProduct () {
 	var addpr = new XMLHttpRequest();
-	var NewProduct = {
+	var newProduct = {
 		name:'',
 		calories:0,
 		proteins:0,
@@ -212,14 +190,15 @@ function addProduct () {
 		group:'',
 		weight:0 
 	};
-	NewProduct.name = document.getElementById('name').value;
-	NewProduct.calories = +document.getElementById('calories').value;
-	NewProduct.proteins = +document.getElementById('proteins').value;
-	NewProduct.fats = +document.getElementById('fats').value;
-	NewProduct.carbohydrates = +document.getElementById('carbohydrates').value;
-	NewProduct.group = document.getElementById('group').value;
-	NewProduct.weight = +document.getElementById('weight').value;
-	var body = JSON.stringify(NewProduct);
+	var formList1 = document.forms[1].elements;
+	newProduct.name = formList1.name.value;
+	newProduct.calories = +formList1.calories.value;
+	newProduct.proteins = +formList1.proteins.value;
+	newProduct.fats = +formList1.fats.value;
+	newProduct.carbohydrates = +formList1.carbohydrates.value;
+	newProduct.group = formList1.group.value;
+	newProduct.weight = 100;
+	var body = JSON.stringify(newProduct);
 
 	console.log(body);
 	
@@ -231,7 +210,12 @@ function addProduct () {
 	}
 	addpr.send(body);
 
-
+	function clerForm() {
+		for (let i=1; i<formList1.length;i++) {
+			formList1[i].value = '';
+		}
+	};
+	clerForm();
 };
 
 addProductButton = document.getElementById("addProductButton");
